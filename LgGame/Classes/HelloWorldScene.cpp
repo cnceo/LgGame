c3f92a3017f50c245d8c0f20556c3e39f6e82e22
xYajun <xialiguo@aliyun.com>
2016-08-17 21:09:33 +0800

    edit fist cpp

--- LgGame/Classes/HelloWorldScene.cpp
+++ LgGame/Classes/HelloWorldScene.cpp
@@ -53,7 +53,6 @@ bool HelloWorld::init()
 
     // add a label shows "Hello World"
     // create and initialize a label
-    
     auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
     
     // position the label on the center of the screen
